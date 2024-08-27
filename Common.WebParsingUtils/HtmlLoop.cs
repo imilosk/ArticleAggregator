03eee7f5d824aea67c5d9d659ef3f68291825c47
miloshkostadinovski <milos.kostadinoski97@gmail.com>
2024-08-27 19:34:43 +0200

    Support javascript

--- Common.WebParsingUtils/HtmlLoop.cs
+++ Common.WebParsingUtils/HtmlLoop.cs
@@ -1,12 +1,14 @@
 ﻿using System.Globalization;
 using System.Xml.XPath;
 using HtmlAgilityPack;
+using PuppeteerSharp;
 
 namespace Common.WebParsingUtils;
 
 public class HtmlLoop
 {
     private readonly HtmlWeb _htmlWeb = new();
+    private IBrowser? _browser;
 
     public async IAsyncEnumerable<IEnumerable<T>> Parse<T>(
         Uri baseUrl,
@@ -17,20 +19,48 @@ public class HtmlLoop
         Func<XPathNavigator, T> delegateAction
     )
     {
-        await Task.CompletedTask;
-        
+        var browser = await InitiateBrowser();
         var currentPage = baseUrl;
+
         do
         {
-            var htmlDocument = _htmlWeb.Load(currentPage);
+            var htmlDocument = await LoadHtmlDocument(browser, currentPage, isJs);
             var rootNode = htmlDocument.DocumentNode ?? throw new Exception("Root element is null");
-
             var items = ScrapePage(rootNode, mainElementXPath, delegateAction);
 
             yield return items;
 
             currentPage = GetNextPageUrl(rootNode, baseUrl, nextPageXPath, cultureInfo);
         } while (currentPage != baseUrl);
+    }
+
+    private async Task<IBrowser> InitiateBrowser()
+    {
+        await new BrowserFetcher().DownloadAsync();
+
+        return _browser ??= await Puppeteer.LaunchAsync(new LaunchOptions
+        {
+            Headless = true
+        });
+    }
+
+    private async Task<HtmlDocument> LoadHtmlDocument(IBrowser browser, Uri url, bool isJs)
+    {
+        if (!isJs)
+        {
+            return _htmlWeb.Load(url);
+        }
+
+        var page = await browser.NewPageAsync();
+        await page.GoToAsync(url.ToString());
+
+        await Task.Delay(1000);
+
+        var htmlContent = await page.GetContentAsync();
+        var htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(htmlContent);
+
+        return htmlDocument;
     }
 
     private static Uri GetNextPageUrl(HtmlNode root, Uri baseUrl, string nextPageXPath, CultureInfo cultureInfo)
