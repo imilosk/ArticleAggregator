8f7afc4b1118dcb026ae7f7af578869b498e8ab7
miloshkostadinovski <milos.kostadinoski97@gmail.com>
2024-09-15 10:08:46 +0200

    Replace WebParsingTools and BaseTypeExtensions with nuget packages

--- Common.WebParsingUtils/StripHtml.cs
+++
@@ -1,9 +0,0 @@
-using System.Text.RegularExpressions;
-
-namespace Common.WebParsingUtils;
-
-public static partial class StripHtml
-{
-    [GeneratedRegex("<.*?>")]
-    public static partial Regex StripHtmlTagsRegex();
-}
